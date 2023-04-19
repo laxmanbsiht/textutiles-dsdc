703bd83e03931c35539a94bc380d0d9e4d6c6aaa
laxman <laxmansingh02082002@gmail..com>
2023-04-19 10:18:01 +0530

    change commit

--- src/component/TextForm.jsx
+++ src/component/TextForm.jsx
@@ -20,7 +20,47 @@ export default function TextForm() {
         onChange={handlechange}
      className="form-control" id="exampleFormControlTextarea1" rows="3">handle</textarea>
     <button onClick={handleClick}  className="btn btn-primary">change to uppercase</button>
+    <div class="container">
+  <h2>Button Groups - Set Sizes</h2>
+  <p>Add class .btn-group-* to size all buttons in a button group.</p>
+  <h3>Large Buttons:</h3>
+  <div class="btn-group btn-group-lg">
+    <button type="button" class="btn btn-primary">Apple</button>
+    <button type="button" class="btn btn-primary">Samsung</button>
+    <button type="button" class="btn btn-primary">Sony</button>
+  </div>
+  <h3>Default Buttons:</h3>
+  <div class="btn-group">
+    <button type="button" class="btn btn-primary">Apple</button>
+    
+    <button type="button" class="btn btn-primary">Samsung</button>
+    <button type="button" class="btn btn-primary">Sony</button>
+  </div>
+  <h3>Small Buttons:</h3>
+  <div class="btn-group btn-group-sm">
+    <button type="button" class="btn btn-primary">Apple</button>
+    <button type="button" class="btn btn-primary">Samsung</button>
+    <button type="button" class="btn btn-primary">Sony</button>
+  </div>
+  <h3>Extra Small Buttons:</h3>
+  <div class="btn-group btn-group-xs">
+    <button type="button" class="btn btn-primary">Apple</button>
+    <button type="button" class="btn btn-primary">Samsung</button>
+    <button type="button" class="btn btn-primary">Sony</button>
+
+    
+      
+    
+    
+
+    
+  </div>
+</div>
 
   </div>
+  
   )
 }
+
+
+
